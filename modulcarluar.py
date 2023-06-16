9e0843bb695c3f3ec2719ad58dd613d46487d0cb
Tiara Vionadita Pattikawa <118671923+Aranadita@users.noreply.github.com>
2023-06-16 21:44:41 +0700

    Update modulcarluar.py

--- modulcarluar.py
+++ modulcarluar.py
@@ -1,16 +1,25 @@
 import json
 
+class ProvinsiTidakTersediaError(Exception):
+    pass
+
+class AsalTidakTersedia(Exception):
+    pass
+
+class TujuanTidakTersedia(Exception):
+    pass
+
 def get_input1():
-    input_provinsi = input("Input provinsi: ")
+    input_provinsi = input("Input Provinsi Asal : ")
     return input_provinsi
 
 def get_input2():
-    input_asal = str(input("Asal : "))
-    input_tujuan = str(input("Tujuan : "))
+    input_asal = input("Kota Asal : ")
+    input_tujuan = input("Kota Tujuan : ")
     return input_asal, input_tujuan
 
 
-def get_harga_layanan(layanan, input_provinsi, input_asal, input_tujuan):
+def layanan_mobil_luar_kota(layanan, input_provinsi, input_asal, input_tujuan):
     with open("daftarharga.json", "r") as file1:
         data1 = json.load(file1)
     
@@ -19,11 +28,19 @@ def get_harga_layanan(layanan, input_provinsi, input_asal, input_tujuan):
     
     provinsi_data = data2.get(input_provinsi)
 
+    if provinsi_data is None:
+        raise ProvinsiTidakTersediaError("Provinsi yang anda input tidak terdapat di Pulau Jawa")
+    
+    if input_asal not in provinsi_data:
+        raise AsalTidakTersedia("Asal tidak tersedia dalam provinsi ini")
+
+    if input_tujuan not in provinsi_data[input_asal]:
+        raise TujuanTidakTersedia("Tujuan tidak tersedia")
+
     if input_provinsi == "Yogyakarta":
         print("Mohon Maaf Layanan Tidak Tersedia Di Provinsi Ini")
         return None
     else:
-
         for item in data1[layanan]:
             if item["layanan"] == "Go Car" or item["layanan"] == "Grab Car" or item["layanan"] == "Car":
                 provinsi_data2 = item["provinsi"]
@@ -37,8 +54,7 @@ def get_harga_layanan(layanan, input_provinsi, input_asal, input_tujuan):
                                 total_harga = harga_minimum
                             else:
                                 total_harga = harga_minimum + (harga_per_km * jarak)
-                                return total_harga
-                        
+                                return total_harga          
     
     raise Exception("Input Anda Tidak Sesuai, Silakan Cek Kembali")
 
@@ -48,18 +64,24 @@ def main():
         input_provinsi = get_input1()
         if input_provinsi == "Yogyakarta":
             print("Mohon Maaf Layanan Tidak Tersedia Di Provinsi Ini")
+
         else:
             input_asal, input_tujuan = get_input2()
-            harga_MaximCar = get_harga_layanan("maxim", input_provinsi, input_asal, input_tujuan)
-            harga_GoCar = get_harga_layanan("gojek", input_provinsi, input_asal, input_tujuan)
-            harga_GrabCar = get_harga_layanan("Grab", input_provinsi, input_asal, input_tujuan)
+            harga_MaximCar = layanan_mobil_luar_kota("maxim", input_provinsi, input_asal, input_tujuan)
+            harga_GoCar = layanan_mobil_luar_kota("gojek", input_provinsi, input_asal, input_tujuan)
+            harga_GrabCar = layanan_mobil_luar_kota("Grab", input_provinsi, input_asal, input_tujuan)
             print("Harga Maxim Car : ", harga_MaximCar)
             print("Harga Go Car : ", harga_GoCar)
             print("Harga Grab Car : ", harga_GrabCar)
 
+    except ProvinsiTidakTersediaError as e:
+        print("Terjadi kesalahan : ", str(e))
+    except AsalTidakTersedia as e:
+        print("Terjadi kesalahan : ", str(e))
+    except TujuanTidakTersedia as e:
+        print("Terjadi kesalahan : ", str(e))
     except Exception as e:
         print("Terjadi kesalahan : ", str(e))
 
 if __name__ == "__main__":
     main()
-    
